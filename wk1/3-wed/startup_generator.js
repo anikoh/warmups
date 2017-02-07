30decc152968e2d84237a2e3f5280cec1d980072
Aniko Hazi <anikoh@gmail.com>
2017-02-08 10:07:04 +1100

    week 1 wed warmup remove unnecessary code

--- wk1/3-wed/startup_generator.js
+++ wk1/3-wed/startup_generator.js
@@ -28,8 +28,6 @@ var secondMax = secondArray.length -1;
 
 
 function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
